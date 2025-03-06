d797459eeb8b56b2b7b67743227ecadbb52d918b
Ciapibara <91439604+Ciapibara@users.noreply.github.com>
2025-03-06 12:16:13 +0100

    ch

---
+++ MVVM/MVVM/Model/Student.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+
+namespace MVVM.Model
+{
+    public class Student : INotifyPropertyChanged
+    {
+        // Pola naszego modelu
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        // Zdarzenie, które służy do powiadomienia o zmianie obiektu
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        // Metoda dzięki której powiadamiamy o zmianie w naszym obiekcie
+        public virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ MVVM/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,48 @@
+﻿
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+using MVVM.Model;
+
+namespace SystemOceniania.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Student> students { get; set; }
+        public Student selectedStudent { get; set; }
+
+        public double TheGrade { get; set; }
+        public Command StudentSelectedCmd { get; }
+
+        public MainViewModel()
+        {
+            students =
+            [
+                new Student() { Id = 1, Name = "Jan", Surname = "Kowalski", },
+                new Student() { Id = 2, Name = "Adam", Surname = "Kaczyński", },
+                new Student() { Id = 3, Name = "Anna", Surname = "Nowak", }
+            ];
+
+            selectedStudent = students[0];
+            StudentSelectedCmd = new Command<Student>(StudentPressed);
+
+        }
+
+        public void StudentPressed(Student student)
+        {
+            if (student != null)
+                selectedStudent = student;
+
+            Application.Current.MainPage.DisplayAlert("Wybrana osoba", $"Witaj {student.Name}!", "OK" );
+            
+
+            OnPropertyChanged(nameof(selectedStudent));
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
